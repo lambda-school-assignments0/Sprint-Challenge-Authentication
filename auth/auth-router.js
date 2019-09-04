a0ec5d3fd2264927fda1e49b7164e35f43d999ea
schang1146 <sammychang2185@gmail.com>
2019-09-03 18:58:06 -0700

    finished writing auth-router.js and authenticate-middleware.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,62 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
-  // implement registration
+const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
+    // implement registration
+    const user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    Users.add(user)
+        .then(res.status(201).json(user))
+        .catch(err =>
+            res
+                .status(500)
+                .json({ message: "Error: Could not register new user", err })
+        );
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+    // implement login
+    const { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = genToken(user);
+
+                res.status(200).json({
+                    message: `Welcome back, ${username}!`,
+                    token
+                });
+            } else {
+                res.status(401).json({ message: "Error: Invalid credentials" });
+            }
+        })
+        .catch(err =>
+            res.status(500).json({ message: "Error: Could not login", err })
+        );
 });
+
+function genToken(user) {
+    const payload = {
+        subject: "username",
+        username: user.username
+    };
+
+    // hardcoded for time constraint
+    const secret = "notsosecretsecret";
+
+    const options = {
+        expiresIn: "1h"
+    };
+
+    return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,24 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
+
+// hardcoded for time constraint
+const secret = "notsosecretsecret";
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+    const token = req.headers.authorization;
+
+    if (token) {
+        jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ message: "Invalid token" });
+            } else {
+                req.decodedJWT = decodedToken;
+                next();
+            }
+        });
+    } else {
+        res.status(401).json({ you: "shall not pass!" });
+    }
 };
