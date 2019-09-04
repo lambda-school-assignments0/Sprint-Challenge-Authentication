b1366f584a63c9065d9e694a70d4c2570fb3ad9b
schang1146 <sammychang2185@gmail.com>
2019-09-03 19:12:13 -0700

    implemented middleware; mvp completed

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization;
 
     if (token) {
-        jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+        jwt.verify(token, secret, (err, decodedToken) => {
             if (err) {
                 res.status(401).json({ message: "Invalid token" });
             } else {
